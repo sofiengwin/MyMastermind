2fd7707369355ca1986b3c0fc447211f39edce89
Ogbara Godwin <sengodwin@gmail.com>
2015-09-18 14:13:05 +0300

    Commiting now working game engine but with minor issue of the congratulatory message for game completion not comming early

--- game.rb
+++ game.rb
@@ -1,3 +1,5 @@
+require_relative 'helper'
+require_relative 'file'
 require 'pry'
 class Game
 attr_accessor :user_name, :user_level
@@ -10,6 +12,7 @@ attr_accessor :user_name, :user_level
   def welcome
     puts Message.welcome
     input = gets.chomp
+    puts ReadFile.test_check(input)
     case input
     when "p"
       choose_level
@@ -49,47 +52,48 @@ end
     until trial_count >= 12
       user_guess = gets.chomp
       userchoice = user_guess.upcase.split("")
-      if @computer_code == @result[:matches]
-          puts "Congratulations you have completed the game in #{trial_count}"
-        @player[:end_time] = Time.now
-        @player[:trial_count] = trial_count
-        #binding.pry
-          break
-        elsif user_guess == "q"
+        if user_guess == "q"
           system(exit)
         elsif user_guess == "c"
           puts @computer_code
         elsif too_long(user_guess) || too_short(user_guess)
             puts "Letter count not enough increase by #{computer_choice_array_length(@player[:level])}"
-      else
+      elsif @computer_code != @result[:matches]
         puts "Try again"
         evaluate_guess(user_guess)
         puts "your choices #{userchoice} matched#{@result[:matches]} is #{trial_count}"
          trial_count += 1
          #binding.pry
+       elsif @computer_code == @result[:matches]
+               puts "Congratulations you have completed the game in #{trial_count}"
+             @player[:end_time] = Time.now
+             @player[:trial_count] = trial_count
+             system(exit)
       end
     end
   end
 
   def evaluate_guess(user_guess)
-    userchoice = user_guess.upcase.split("")
-    combined_array = @computer_code.zip(userchoice)
-    @combined_array_hashed = {}
-    combined_array.each do |array|
-        @combined_array_hashed[array[0]] = array[1]
-    end
-    #binding.pry
-    @combined_array_hashed.each do |key, value|
-      if key == value
-        @result[:matches] << value
-      elsif userchoice.include? key
-        @result[:nomatches] << value
+      @result[:matches] = []
+      @result[:nomatches] = []
+      @result[:missed_matches] = []
+      userchoice = user_guess.upcase.split("")
+      combined_array = @computer_code.zip(userchoice)
+      matches_array = combined_array.select do |each_array|
+     if each_array[0] == each_array[1]
+         @result[:matches] << each_array[0]
+     end
+ end
+    partial_array = combined_array - matches_array
+    partial_array.each do |each_array|
+      if userchoice.include? each_array[0]
+          @result[:nomatches] << each_array[0]
       else
-      @result[:missed_matches] << value
+          @result[:missed_matches] << each_array[0]
       end
     end
     @result
-  end
+end
 
   def too_long(user_guess)
       user_guess.length > computer_choice_array_length(@player[:level])
@@ -97,6 +101,17 @@ end
 
   def too_short(user_guess)
     user_guess.length < computer_choice_array_length(@player[:level])
+  end
+
+  def gather_details
+    name = @player[:name]
+    computer_code = @computer_code
+    end_time = @player[:end_time]
+    start_time = @player[:start_time]
+    trial_count = @player[:trial_count]
+    ReadFile.add_record(name, computer_code, end_time, start_time, trial_count)
+    ReadFile.show_top_ten
+    ReadFile.save_new_top_ten
   end
 
   def computer_choice_array_length(player)
@@ -118,54 +133,6 @@ end
     return 4
     end
   end
-end
-
-class Message
-    def self.welcome
-        %q{Would you like to (p)lay, read the (i)nstructions,
-          read a little (b)ackground on MasterMind or (q)uit?}
-    end
-
-    def self.choose_level
-     %q{To start the game select a level you like to play
-
-        Enter 1 for Beginner
-        Enter 2 for Intermediate
-        Enter 3 for Advanced}
-    end
-
-    def sequence_generated_message
-      puts %q{A sequence has been generated for you try to match them by addiing
-      your guesses below}
-    end
-
-    def self.show_background
-        %q{This MasterMind game is a great game and you will never
-        defeat me}
-    end
-
-    def self.enter_name
-      %q{Please add you name to proceed}
-    end
-
-    def self.good_bye
-      "this is a test"
-    end
-
-    def self.is_invalid
-    puts  %q{Your entry is invalid. Please folow the instructions
-      provided to fully enjoy the game}
-    end
-
-    def  self.generated_sequence
-  %q{I have generated a beginner sequence with four elements made up of:
-(o)range, (c)yan, (i)ndigo and (r)ed. You are to guess the sequence in which the
-se colors appeared e.g OCIR for (o)range, (c)yan, (i)ndigo and (r)ed. Use (q)uit at any time to en
-d the game.
-Ready to play?
-What's your guess?}
-  end
-
 end
 
 
